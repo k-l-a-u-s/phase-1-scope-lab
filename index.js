a35de070e9a5eb9c55c863c2a50378d2df1185c3
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-08-28 11:24:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+function setBestCustomer(){
+    window.bestCustomer;
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+
+
+   const leastFavoriteCustomer = 'bob'
+
+
+
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'bob'
+    
+}
